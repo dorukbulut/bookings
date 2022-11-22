b10ef73457ddab2769af9ce48fed1115076e9298
Doruk Bulut <dorukbulutinfo@gmail.com>
2022-11-23 00:55:25 +0300

    Reservation Summary Page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -97,7 +97,11 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 			return
 
+		} else {
+			m.App.Session.Put(r.Context(), "reservation", reservation)
+			http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
 		}
+
 	}
 
 }
@@ -152,4 +156,21 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 // Contact renders the contact page
 func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, r, "contact.page.tmpl", &models.TemplateData{})
+}
+
+// ReservationSummary renders the reservation summary page.
+func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) {
+	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	if !ok {
+		log.Println("cannot get item from session")
+		m.App.Session.Put(r.Context(), "error", "Can't get reservation from session")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	m.App.Session.Remove(r.Context(), "reservation")
+	data := make(map[string]interface{})
+	data["reservation"] = reservation
+	render.RenderTemplate(w, r, "reservation-summary.page.tmpl", &models.TemplateData{
+		Data: data,
+	})
 }
